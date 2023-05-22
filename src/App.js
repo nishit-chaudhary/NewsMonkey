3463c12a561f5a0c696f777045f282abf9152c6d
Nishit <nishitchaudhary706@gmail.com>
2023-05-22 14:35:48 +0530

    converted from class-based to function-based

--- src/App.js
+++ src/App.js
@@ -1,42 +1,38 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import Navbar from './components/Navbar';
 import News from './components/News';
 import LoadingBar from 'react-top-loading-bar';
 
-export default class App extends Component {
-  pageSize = 10;
-  apiKey = process.env.REACT_APP_NEWS_API;
+const App = () => {
+  const pageSize = 10;
+  const apiKey = process.env.REACT_APP_NEWS_API;
 
-  state = {
-    progress: 0
-  }
+  const [progress, setProgress] = useState(0)
 
-  setProgress = (progress) => {
-    this.setState({progress: progress})
-  }
-  render() {
     return (
       <div>
         <Router>
         <Navbar />
         <LoadingBar
           color='red'
-          progress={this.state.progress}
+          progress={progress}
         />
         <Routes>
-          <Route exact path='/' element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="general" pageSize={this.pageSize} country="in" category="general" />} ></Route>
-          <Route exact path='/business' element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="business" pageSize={this.pageSize} country="in" category="business" />} ></Route>
-          <Route exact path='/entertainment' element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="entertainment" pageSize={this.pageSize} country="in" category="entertainment" />} ></Route>
-          <Route exact path='/general' element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="general" pageSize={this.pageSize} country="in" category="general" />} ></Route>
-          <Route exact path='/health' element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="health" pageSize={this.pageSize} country="in" category="health" />} ></Route>
-          <Route exact path='/science' element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="science" pageSize={this.pageSize} country="in" category="science" />} ></Route>
-          <Route exact path='/sports' element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="sports" pageSize={this.pageSize} country="in" category="sports" />} ></Route>
-          <Route exact path='/technology' element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="technology" pageSize={this.pageSize} country="in" category="technology" />} ></Route>
+          <Route exact path='/' element={<News setProgress={setProgress} apiKey={apiKey} key="general" pageSize={pageSize} country="in" category="general" />} ></Route>
+          <Route exact path='/business' element={<News setProgress={setProgress} apiKey={apiKey} key="business" pageSize={pageSize} country="in" category="business" />} ></Route>
+          <Route exact path='/entertainment' element={<News setProgress={setProgress} apiKey={apiKey} key="entertainment" pageSize={pageSize} country="in" category="entertainment" />} ></Route>
+          <Route exact path='/general' element={<News setProgress={setProgress} apiKey={apiKey} key="general" pageSize={pageSize} country="in" category="general" />} ></Route>
+          <Route exact path='/health' element={<News setProgress={setProgress} apiKey={apiKey} key="health" pageSize={pageSize} country="in" category="health" />} ></Route>
+          <Route exact path='/science' element={<News setProgress={setProgress} apiKey={apiKey} key="science" pageSize={pageSize} country="in" category="science" />} ></Route>
+          <Route exact path='/sports' element={<News setProgress={setProgress} apiKey={apiKey} key="sports" pageSize={pageSize} country="in" category="sports" />} ></Route>
+          <Route exact path='/technology' element={<News setProgress={setProgress} apiKey={apiKey} key="technology" pageSize={pageSize} country="in" category="technology" />} ></Route>
         </Routes>
         </Router>
       </div>
     )
-  }
+
 }
+
+export default App;
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,28 +1,26 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export class NewsItem extends Component {
+const NewsItem = (props) => {
 
-    render() {
-        let { title, description, imageUrl, newsUrl, author, date, source } = this.props;
-        return (
-            <div className='my-3'>
-                <div className="card">
-                    <div className='d-flex justify-content-end position-absolute end-0'>
-
-                    <span className="badge rounded-pill bg-danger" style={{left: '90%', zIndex: '1'}}>
+    let { title, description, imageUrl, newsUrl, author, date, source } = props;
+    return (
+        <div className='my-3'>
+            <div className="card">
+                <div className='d-flex justify-content-end position-absolute end-0'>
+                    <span className="badge rounded-pill bg-danger" style={{ left: '90%', zIndex: '1' }}>
                         {source} </span>
-                    </div>
-                    <img src={imageUrl} className="card-img-top" alt="..." />
-                    <div className="card-body">
-                        <h5 className="card-title">{title} </h5>
-                        <p className="card-text">{description}</p>
-                        <p className="card-text"><small className="text-muted">By {author ? author : "Unknown"} on {new Date(date).toGMTString()} </small></p>
-                        <a rel='noreferrer' href={newsUrl} target='_blank' className="btn btn-sm btn-dark">Read More</a>
-                    </div>
+                </div>
+                <img src={imageUrl} className="card-img-top" alt="..." />
+                <div className="card-body">
+                    <h5 className="card-title">{title} </h5>
+                    <p className="card-text">{description}</p>
+                    <p className="card-text"><small className="text-muted">By {author ? author : "Unknown"} on {new Date(date).toGMTString()} </small></p>
+                    <a rel='noreferrer' href={newsUrl} target='_blank' className="btn btn-sm btn-dark">Read More</a>
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
+
 }
 
 export default NewsItem
